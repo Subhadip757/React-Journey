73501d306e18ff31353e6e04f1f67ebfc73bfc53
Subhadip757 <SubhadipGitHub@gmail.com>
2024-03-17 14:10:43 +0530

    new project

---
+++ Mini Context/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './Components/Login'
+import Profile from './Components/Profile'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <UserContextProvider>
+      <h1>React Journey</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
